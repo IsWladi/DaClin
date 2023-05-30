52f9102117be68e0a89df0b801b12b54fdb477cf
wladi <wladimir2000@protonmail.com>
2023-05-30 15:26:19 -0400

    tab4 agregado y cambio tittle en tabs

--- FichaClinica/src/app/app-routing.module.ts
+++ FichaClinica/src/app/app-routing.module.ts
@@ -9,6 +9,10 @@ const routes: Routes = [
   {
     path: 'tabs', // url = http://localhost:8100/tabs/tabs/tab1
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'tab4',
+    loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
   }
 ];
 @NgModule({
