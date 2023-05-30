c9c039440d6225e3ae1c3e36a94e891988a352a6
wladi <wladimir2000@protonmail.com>
2023-05-30 13:56:26 -0400

    page de login agregada, ruta '/' en login por defecto

--- FichaClinica/src/app/app-routing.module.ts
+++ FichaClinica/src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'tabs', // url = http://localhost:8100/tabs/tabs/tab1
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   }
 ];
