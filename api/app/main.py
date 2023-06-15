22bc5dcba16eb16dd04657f37058cfd904afba37
wladi <wladimir2000@protonmail.com>
2023-06-14 22:20:50 -0400

    endpoint crear cita tab citas

--- api/app/main.py
+++ api/app/main.py
@@ -15,6 +15,11 @@ class UserRegistration(BaseModel):
     username: str
     password: str
 
+class Cita(BaseModel):
+    motivo: str
+    especialidad: str
+    fecha: str
+
 
 app = FastAPI()
 
@@ -48,6 +53,16 @@ mongo_client = MongoClient("mongodb://DaClin_bd:27017/",
 mongo_db = mongo_client["DaClin"]
 usuarios_collection = mongo_db["usuarios"]
 
+# para testear
+
+@app.get("/api/users/get/{setting}")
+async def get_user(setting:str):
+    if setting == "all":
+         return json.loads(dumps(usuarios_collection.find()))
+    else:
+        usuario = usuarios_collection.find_one({"username":setting})
+        return json.loads(dumps(usuario))
+
 # post para crear un usuario
 
 @app.post("/api/users/register/")
@@ -75,6 +90,25 @@ async def login_user(user: UserRegistration):
     else:
         return False
 
+# endpoints para tabs 1 2 3 4
+
+# crear nueva cita
+@app.post("/api/citas/agregar/{username}")
+async def agregar_cita(cita: Cita, username: str):
+    usuario = usuarios_collection.find_one({"username": username})
+    if usuario:
+        citas = usuario.get("citas", {})  # Obtenemos la lista de citas existentes o una lista vacía
+        if cita.motivo in citas:
+            return {"message": "motivo ya existente"}
+        else:
+            citas[f"{cita.motivo}"] = {"especialidad":cita.especialidad, "fecha":cita.fecha}
+            usuarios_collection.update_one(
+                {"username": username},
+                {"$set": {"citas": citas}}  # Actualizamos el campo "citas" con la lista actualizada
+            )
+            return {"message": "Cita agregada exitosamente"}
+    else:
+        return {"message": "Usuario no encontrado"}
 
 @app.get("/")
 async def root():
